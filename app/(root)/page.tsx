b08cd396bd94392524da7e263e3c5a5a9521247b
samikhan098 <samee6932@gmail.com>
2024-10-11 18:42:42 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import AddDocumentBtn from '@/components/AddDocumentBtn';
 import { DeleteModal } from '@/components/DeleteModal';
 import Header from '@/components/Header'
+import Notifications from '@/components/Notification';
 import { getDocuments } from '@/lib/actions/room.action';
 import { dateConverter } from '@/lib/utlis';
 import { SignedIn, UserButton } from '@clerk/nextjs'
@@ -19,7 +20,7 @@ const Home = async () => {
     <main className="home-container">
       <Header className="sticky left-0 top-0">
         <div className="flex items-center gap-2 lg:gap-4">
-          Notifications 
+          <Notifications/> 
           <SignedIn>
             <UserButton />
           </SignedIn>
