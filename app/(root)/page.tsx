87a4ebaebedb743d591ab889cb5fa8b0c0a4096f
samikhan098 <samee6932@gmail.com>
2024-10-11 18:01:18 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import AddDocumentBtn from '@/components/AddDocumentBtn';
+import { DeleteModal } from '@/components/DeleteModal';
 import Header from '@/components/Header'
 import { getDocuments } from '@/lib/actions/room.action';
 import { dateConverter } from '@/lib/utlis';
@@ -51,7 +52,7 @@ const Home = async () => {
                     <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
                   </div>
                 </Link>
-                
+                <DeleteModal roomId={id}/>
               </li>
             ))}
           </ul>
